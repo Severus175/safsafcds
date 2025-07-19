e652ec5c5cdc77dd38aabab4189bdd48dbe1da71
Severus175 <severus175@gmail.com>
2025-07-19 21:53:11 +0530

    Fix ShineBorder Component - Exact Implementation

--- src/components/ui/ShineBorder.tsx
+++ src/components/ui/ShineBorder.tsx
@@ -22,7 +22,7 @@ export default function ShineBorder({
         '--background-radial-gradient': `radial-gradient(transparent,transparent,${color[0]},${color[1]},${color[2]},transparent,transparent)`,
       } as React.CSSProperties}
     >
-      <div className="before:absolute before:inset-0 before:aspect-square before:size-full before:rounded-[--border-radius] before:p-[--border-width] before:will-change-[background-position] before:content-[''] before:![-webkit-mask-composite:xor] before:![mask-composite:exclude] before:[background-image:--background-radial-gradient] before:[background-size:300%_300%] before:[mask:--mask-linear-gradient] motion-safe:before:animate-shine relative z-10 w-full h-full">
+      <div className="before:absolute before:inset-0 before:aspect-square before:size-full before:rounded-[--border-radius] before:p-[--border-width] before:will-change-[background-position] before:content-[''] before:![-webkit-mask-composite:xor] before:![mask-composite:exclude] before:[background-image:--background-radial-gradient] before:[background-size:300%_300%] before:[mask:--mask-linear-gradient] motion-safe:before:animate-shine">
         {children}
       </div>
     </div>
